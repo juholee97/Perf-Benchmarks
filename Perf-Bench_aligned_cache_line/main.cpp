63572171df841a43020238c98fd91624590221b4
juholee97 <156589691+juholee97@users.noreply.github.com>
2024-08-06 00:43:55 +0900

    fix : some of code line.

--- Perf-Bench_aligned_cache_line/main.cpp
+++ Perf-Bench_aligned_cache_line/main.cpp
@@ -11,41 +11,48 @@ std::ofstream outputFile( "compare_performance.csv" );
 const int NUM_THREADS = 4;
 const int NUM_INCREMENTS = 1000000;
 
-struct AlignedAtomic {
+struct AlignedAtomic 
+{
 	alignas( std::hardware_destructive_interference_size ) std::atomic<int> value;
 };
 
-struct NonAlignedAtomic {
+struct NonAlignedAtomic 
+{
 	std::atomic<int> value;
 };
 
 AlignedAtomic alignedCounters[ NUM_THREADS ];
 NonAlignedAtomic nonAlignedCounters[ NUM_THREADS ];
 
-void incrementPadded( int index ) {
-	for ( int i = 0; i < NUM_INCREMENTS; ++i ) {
+void incrementPadded( int index ) 
+{
+	for ( int i = 0; i < NUM_INCREMENTS; ++i ) 
+	{
 		alignedCounters[ index ].value.fetch_add( 1, std::memory_order_relaxed );
 	}
 }
 
 void incrementNonPadded( int index ) {
-	for ( int i = 0; i < NUM_INCREMENTS; ++i ) {
+	for ( int i = 0; i < NUM_INCREMENTS; ++i ) 
+	{
 		nonAlignedCounters[ index ].value.fetch_add( 1, std::memory_order_relaxed );
 	}
 }
 
-void measureAndOutputPerformance() {
-
+void measureAndOutputPerformance() 
+{
 	// Measure performance with alignas
 	{
 		std::vector<std::thread> threads;
 		auto start = std::chrono::high_resolution_clock::now();
 
-		for ( int i = 0; i < NUM_THREADS; ++i ) {
+		for ( int i = 0; i < NUM_THREADS; ++i ) 
+		{
 			threads.emplace_back( incrementPadded, i );
 		}
 
-		for ( auto& t : threads ) {
+		for ( auto& t : threads ) 
+		{
 			t.join();
 		}
 
@@ -60,11 +67,13 @@ void measureAndOutputPerformance() {
 		std::vector<std::thread> threads;
 		auto start = std::chrono::high_resolution_clock::now();
 
-		for ( int i = 0; i < NUM_THREADS; ++i ) {
+		for ( int i = 0; i < NUM_THREADS; ++i ) 
+		{
 			threads.emplace_back( incrementNonPadded, i );
 		}
 
-		for ( auto& t : threads ) {
+		for ( auto& t : threads ) 
+		{
 			t.join();
 		}
 
@@ -76,8 +85,9 @@ void measureAndOutputPerformance() {
 
 }
 
-int main() {
-	for ( int i = 0; i < 1000; i++ )
+int main() 
+{
+	for ( int i = 0; i < 1000; ++i )
 	{
 		measureAndOutputPerformance();
 	}
